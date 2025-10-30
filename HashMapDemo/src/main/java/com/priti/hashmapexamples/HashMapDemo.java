aec74607ce56f29dc8254aaffe8e4a57deb0e9df
Priti Kumari <151085priti@gmail.com>
2025-10-30 22:59:58 +0800

    code commit

--- HashMapDemo/src/main/java/com/priti/hashmapexamples/HashMapDemo.java
+++ HashMapDemo/src/main/java/com/priti/hashmapexamples/HashMapDemo.java
@@ -13,14 +13,24 @@ public class HashMapDemo {
         hashmap.put(3,"Cat");
 
         System.out.println("hashmap : "+hashmap +"\n");  //hashmap : {1=Apple, 2=Ball, 3=Cat}
-
+//entryset
         Set<Map.Entry<Integer,String>> entrysets  = hashmap.entrySet();
 
         System.out.println("entrysets : "+entrysets + "\n"); // entryset : [1=Apple, 2=Ball, 3=Cat]
         System.out.println(
                 "Key" + "|"+"value"
         );
-        for(Map.Entry<Integer, String > entryset : entrysets)
-        System.out.println(entryset.getKey() + "|"+ entryset.getKey());
+        for(Map.Entry<Integer, String > entryset : entrysets){
+            System.out.println(entryset.getKey() + "|"+ entryset.getKey());
+        }
+
+
+        //keyset
+        for(Integer key : hashmap.keySet())
+        {
+           /* System.out.println("key : "+key);
+            System.out.println("\n");*/
+            System.out.println("key : "+key +"," +" value : "+hashmap.get(key));
+        }
     }
 }
